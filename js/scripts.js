dd9947fa7c179826f53e72d0e8b6c67ed665379a
YcleptInsan <nickmwise@gmail.com>
2017-04-28 10:41:09 -0700

    Add event handler to hide class main-head on click of #first-clue.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,10 @@
-
-
-
-
 $(document).ready(function(){
-	$(".form-group").submit(function("event"){
+	$("#first-clue").click(function(event){
 		event.preventDefault();
+		$(".main-header").hide();
+		$("#first-clue").css({
+			"background-color: white"
+		});
 
-		var pickNum = $("input#Q1").val();
-			if (pickNum <= 100);
-				.show("#success");
-				.hide("show/hide1"); {
-					else ()
-				}
-		var favColor = $("input#Q2").val();
-		var marajaunaQuestion = $("input#Q3").val();
-
-		$(".output").return("result");
 	});
 });
