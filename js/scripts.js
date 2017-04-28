05951f7a01495168bf9f3c7df346cc87edccb492
YcleptInsan <nickmwise@gmail.com>
2017-04-28 16:10:59 -0700

    Add all 3 surveys with 3 different options.Gather input from radio values as on or off states for back end logic later. all results from will be put in a seprate var to be displayed in the output window.

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,34 @@ $(document).ready(function(){
 		$(".radios-1stSet").show();
 		$("#hidden-header").hide();
 	});
+
+	$("#button1").click(function(event){
+		event.preventDefault();
+		$(".radios-1stSet").hide();
+		$(".radios-2ndSet").show();
+		var radio1Result= [radio1Set1 + radio2Set1 + radio3Set1];
+		var radio1Set1 = $("input#radio1").val();
+		var radio2Set1 = $("input#radio2").val();
+		var radio3Set1 = $("input#radio3").val();
+
+	});
+	$("#button2").click(function(event){
+		event.preventDefault();
+		$(".radios-2ndSet").hide();
+		$(".radios-3rdSet").show();
+		var radio1Set2 = $("input#radio1Set2").val();
+		var radio2Set2 = $("input#radio2Set2").val();
+		var radio3Set2 = $("input#radio3Set2").val();
+
+	});
+	$("#button3").click(function(event){
+		event.preventDefault();
+		$(".radios-3rdSet").hide();
+		$(".container").show();
+		$(".output").show();
+		$(".output").append(result);
+		var radio1Set3 = $("input#radio1Set3").val();
+		var radio2Set3 = $("input#radio2Set3").val();
+		var radio3Set3 = $("input#radio3Set3").val();
+	});
 });
