12b9ca7670cfabf509632071af87019f4518bf0a
Andrew Glines and nick wise <nick@>
2017-05-03 13:00:25 -0700

    Change value sates to look for only radios checked.

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,52 @@
-function on__off_Val1(){
+function on__off_Val1() {
 
-	var main_result = $(radio1Set1)
-	 document.getElementById("radio1").value;
-    document.getElementById(".output").innerHTML = main_result;
-		if (radio1Set1 === on)
-			alert("on");
-	// var main_result = radioSet1Result + radioSet2Result + radioSet3Result;
 }
+
 $(document).ready(function(){
 	$("#first-clue-event1").click(function(event){
-		event.preventDefault();
-		$(".main-header").hide();
-		$(".bg-toggle").addClass("bg-white");
-		$("#hidden-header").show();
+			event.preventDefault();
+				$(".main-header").hide();
+				$(".bg-toggle").addClass("bg-white");
+				$("#hidden-header").show();
 	});
 
 	$("#event2").click(function(event){
 		event.preventDefault();
-		$("#first-clue-event1").hide();
-		$(".radios-1stSet").show();
-		$("#hidden-header").hide();
+			var result1 = $("input:radio[name=radio]:checked").val();
+				$("#first-clue-event1").hide();
+				$(".radios-1stSet").show();
+				$("#hidden-header").hide();
+
+				if( result1 === undefined) {
+					
+				}
+
+
+
 	});
 
 	$("#button1").click(function(event){
 		event.preventDefault();
-		$(".radios-1stSet").hide();
-		$(".radios-2ndSet").show();
-		var radioSet1Result= (radio1Set1 + radio2Set1 + radio3Set1);
-		var radio1Set1 = $("input#radio1").val();
-		var radio2Set1 = $("input#radio2").val();
-		var radio3Set1 = $("input#radio3").val();
+			var result2 = $("input:radio[name=radio2]:checked").val();
+				$(".radios-1stSet").hide();
+				$(".radios-2ndSet").show();
+				console.log(result2);
 
 	});
 	$("#button2").click(function(event){
 		event.preventDefault();
-		$(".radios-2ndSet").hide();
-		$(".radios-3rdSet").show();
-		var radioSet2Result= (radio1Set2 + radio2Set2 + radio3Set2);
-		var radio1Set2 = $("input#radio1Set2").val();
-		var radio2Set2 = $("input#radio2Set2").val();
-		var radio3Set2 = $("input#radio3Set2").val();
+			var result3 = $("input:radio[name=radio3]:checked").val();
+				$(".radios-2ndSet").hide();
+				$(".radios-3rdSet").show();
+				console.log(result3);
 
 	});
 	$("#button3").click(function(event){
-		event.preventDefault();
-		$(".radios-3rdSet").hide();
-		$(".container").show();
-		$(".output").show();
-		$(".output").append(main_result);
-		var radioSet3Result= (radio1Set3 + radio2Set3 + radio3Set3);
-		var radio1Set3 = $("input#radio1Set3").val();
-		var radio2Set3 = $("input#radio2Set3").val();
-		var radio3Set3 = $("input#radio3Set3").val();
+			event.preventDefault();
+			$(".radios-3rdSet").hide();
+			$(".container").show();
+			$(".output").show();
+			$(".output").append(result);
+
 	});
 });
