99e216695799d98103aa621e2951574b7dc725f2
YcleptInsan <nickmwise@gmail.com>
2017-05-09 11:41:43 -0700

    Add if else statements to user logic that show and hide differnt results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+// // BACKEND LOGIC
+// var result = function(radio1, radio2, radio3) {
+// 	radio1 =
+// 	radio2 =
+// 	radio3 =
+// } else {
+//
+// }
+
 // FRONT END LOGIC
 $(document).ready(function(){
 	//ON CLICK OF TEXT ID FOR FIRST CLUE HIDE MAIN HEADER AMD ADD BACGROUND CLASS FOR THE HIDDEN TEXT. LAST SHOW THE HEADER TO PROMPT USER OF SURVEY INSTRUCTIONS
@@ -19,10 +28,12 @@ $(document).ready(function(){
 	//ON SUBMIT OF FIRST QUESTIOn  HIDE CURRENT QUESTIOn AND SHOW NEXT QUESTIOn//
 	$("#button3").click(function(){
 		$(".container").show();
+		$(".output").show();
 		$(".radios-1stSet").hide();
 		$(".radios-2ndSet").hide();
 		$(".radios-3rdSet").hide();
-
+			
+			var msg = "please select at least one"
 			//GET RADIO VALUES AND STOER THEM IN  VAIABLES TO BE USED BELOW
 			var radio1 = document.getElementById("radio1").checked;
 			console.log(radio1);
@@ -30,6 +41,16 @@ $(document).ready(function(){
 			console.log(radio2);
 			var radio3 = document.getElementById("radio3").checked;
 			console.log(radio3);
+
+					if(radio1 === true) {
+						$("#imgResult1").show()
+					}else if(radio2 === true){
+						$("#imgResult2").show()
+					}else if(radio3 === true){
+						$("#imgResult3").show()
+					}else {
+						alert(msg);
+					}
 
 	    //GATHERS AND STORES INPUT OF SECOND QUESTIOn IN VARIABLES//
 			var radio1Set2 = document.getElementById("radio1Set2").checked;
@@ -39,6 +60,16 @@ $(document).ready(function(){
 			var radio3Set2 = document.getElementById("radio3Set2").checked;
 			console.log(radio3Set2);
 
+					if(radio1Set2 === true) {
+						$("#imgResult2").show()
+					} else if(radio2Set2 === true){
+						$("#imgResult1").show()
+					} else if(radio3Set2 === true){
+						$("#imgResult3").show()
+					} else{
+						alert(msg);
+					}
+
 			var radio1Set3 = document.getElementById("radio1Set3").checked;
 			console.log(radio1Set3);
 			var radio2Set3 = document.getElementById("radio2Set3").checked;
@@ -46,9 +77,15 @@ $(document).ready(function(){
 			var radio3Set3 = document.getElementById("radio3Set3").checked;
 			console.log(radio3Set3);
 
-			 if (raido1 === true) {
-
-			 }
+					if(radio1Set3 === true) {
+						$("#imgResult3").show()
+					} else if(radio2Set3 === true){
+						$("#imgResult2").show()
+					} else if(radio3Set3 === true){
+						$("#imgResult1").show()
+					} else{
+						alert(msg);
+					}
 
 	   });
 	  });
