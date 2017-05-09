1028e8ca2fcd3488460f7de88fc8b22fb00180bf
YcleptInsan <nickmwise@gmail.com>
2017-05-09 09:54:50 -0700

    Add New Layout in html with only one screen for questions and only one submit button. Refactor User logic in javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -10,68 +10,47 @@ $(document).ready(function(){
 			//ON CLICK OF EVENT 2  TRIGGERS ALL CONTENT BEFORE TO BE HIDDEN AND THEM SHOW THE FIRST QUESION
 	$("#event2").click(function(){
 
-			$("#first-clue-event1").hide();
-			$(".radios-1stSet").show();
-			$("#hidden-header").hide();
+		$("#first-clue-event1").hide();
+		$("#hidden-header").hide();
+		$(".radios-1stSet").show();
+		$(".radios-2ndSet").show();
+		$(".radios-3rdSet").show();
 
-			//ON SUBMIT OF FIRST QUESTIOn  HIDE CURRENT QUESTIOn AND SHOW NEXT QUESTIOn//
-	$("#button1").click(function(){
-			$(".radios-1stSet").hide();
-			$(".radios-2ndSet").show();
-			//GET RADIO VALUES AND STOER THEM IN  VAIABLES
-				var radio1 = document.getElementById("radio1").checked;
-				console.log(radio1);
-				var radio2 = document.getElementById("radio2").checked;
-				console.log(radio2);
-				var radio3 = document.getElementById("radio3").checked;
-				console.log(radio3);
-				// IF ELSE STAEMENTS TO TRIGGER SHOWING OF PICTURES BASED ON RADIO VALUE AS A BOOLEEN
-					if(radio1 === true && radio2 === false && radio3 === false || radio1Set2 === true && radio2Set2 === false && radio3Set2 === false || radio1Set3 === true && radio2Set3 === false && radio3Set3 === false) {
+	//ON SUBMIT OF FIRST QUESTIOn  HIDE CURRENT QUESTIOn AND SHOW NEXT QUESTIOn//
+	$("#button3").click(function(){
+		$(".container").show();
+		$(".radios-1stSet").hide();
+		$(".radios-2ndSet").hide();
+		$(".radios-3rdSet").hide();
 
-						$("#imgResult1").show();
-					}else {
-						alert("do something");
-					}
+			//GET RADIO VALUES AND STOER THEM IN  VAIABLES TO BE USED BELOW
+			var radio1 = document.getElementById("radio1").checked;
+			console.log(radio1);
+			var radio2 = document.getElementById("radio2").checked;
+			console.log(radio2);
+			var radio3 = document.getElementById("radio3").checked;
+			console.log(radio3);
 
-					//ON CLICK OF BUTTON FOR SECOND QUESTIOn HIDE CURRENT QUESTIOn AND SHOW NEXT QUESTIOn//
-		$("#button2").click(function(){
-			$(".radios-2ndSet").hide();
-			$(".radios-3rdSet").show();
+	    //GATHERS AND STORES INPUT OF SECOND QUESTIOn IN VARIABLES//
+			var radio1Set2 = document.getElementById("radio1Set2").checked;
+			console.log(radio1Set2);
+			var radio2Set2 = document.getElementById("radio2Set2").checked;
+			console.log(radio2Set2);
+			var radio3Set2 = document.getElementById("radio3Set2").checked;
+			console.log(radio3Set2);
 
-			//GATHERS AND STORES INPUT OF SECOND QUESTIOn IN VARIABLES//
-				var radio1Set2 = document.getElementById("radio1Set2").checked;
-				console.log(radio1Set2);
-				var radio2Set2 = document.getElementById("radio2Set2").checked;
-				console.log(radio2Set2);
-				var radio3Set2 = document.getElementById("radio3Set2").checked;
-				console.log(radio3Set2);
-				//IF SAND ELSE STATEMENTS FOR DETERMINING IMAGE RESULT SHOWN
-				if(radio1 === false && radio2 === true && radio3 === false || radio1Set2 === false && radio2Set2 === true && radio3Set2 === false || radio1Set3 === false && radio2Set3 === true && radio3Set3 === false || radio2=== true && radio3Set2 ===true && radio3Set3 === true) {
+			var radio1Set3 = document.getElementById("radio1Set3").checked;
+			console.log(radio1Set3);
+			var radio2Set3 = document.getElementById("radio2Set3").checked;
+			console.log(radio2Set3);
+			var radio3Set3 = document.getElementById("radio3Set3").checked;
+			console.log(radio3Set3);
 
-					$("#imgResult2").show();
+			 if (raido1 === true) {
 
-			} else {
-				$("#imgResult1").show();
-				$("#imgResult2").hide();
-				$("#imgResult3").hide();
-			}
-				//LAST BUTTON ON CLICK HIDES QUESTIOn 3  and shows container.
-		$("#button3").click(function(){
+			 }
 
-			$(".radios-3rdSet").hide();
-			$(".container").show();
-
-				var radio1Set3 = document.getElementById("radio1Set3").checked;
-				var radio2Set3 = document.getElementById("radio2Set3").checked;
-				var radio3Set3 = document.getElementById("radio3Set3").checked;
-
-				if(radio1 === false && radio2 === false && radio3 === true || radio1Set2 === false && radio2Set2 === false && radio3Set2 === true || radio1Set3 === false && radio2Set3 === false && radio3Set3 === true) {
-
-					$("#imgResult3").show();
-				}
-		});
-	  });
-	  });
+	   });
 	  });
 	 });
   });
